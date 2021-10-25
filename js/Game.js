e3142f0493ac689844cd4f10e6610311209f1f06
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2021-10-25 18:07:51 +0800

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,51 @@
+class Game {
+  constructor(){
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+  
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game start", 120, 100);
+    Player.getPlayerInfo();
+    console.log(allPlayers)
+    if (allPlayers !== undefined) {
+      displayPosition = 130;
+      for (var plr in allPlayers) {
+        if (plr === "player"+player.index) {
+          fill("red");
+        } else{
+          fill("black");
+        }
+        displayPosition+=40;
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120, displayPosition)
+      }
+
+    }
+    if (keyDown(UP_ARROW)&&player.index!==null) {
+      player.distance+=50;
+      player.update();
+    }
+  }
+}
